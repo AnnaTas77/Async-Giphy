5ee415ae15e9df0b8c256542b881636ff948ce62
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-29 15:58:47 +0100

    Created a getRandomCatImage function as part of the Bonus task

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const endpointCat = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+const endpointDog = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
 async function getCatImages(query) {
   const response = await fetch(query);
@@ -11,8 +12,6 @@ async function getCatImages(query) {
   console.log("Cat image: ", singleGifImageURL);
 }
 
-const endpointDog = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
-
 async function getDogImages(query) {
   const response = await fetch(query);
   const responseData = await response.json();
@@ -22,8 +21,31 @@ async function getDogImages(query) {
   console.log("Dog image: ", singleGifImageURL);
 }
 
+//BONUS
+
+const allCatsArray = [];
+
+async function getRandomCatImage(query) {
+  const response = await fetch(query);
+  const responseData = await response.json();
+
+  const randomNum = Math.floor(Math.random() * 25);
+
+  const responseArray = responseData.data;
+  responseArray.forEach((cat) => {
+    allCatsArray.push(cat);
+  });
+
+  //   console.log(allCatsArray.length)
+
+  const randomCatImageURL = responseData.data[randomNum].url;
+
+  console.log("Random cat image: ", randomCatImageURL);
+}
+
 getCatImages(endpointCat);
 getDogImages(endpointDog);
+getRandomCatImage(endpointCat);
 
 // Print out value of API key stored in .env file
 // console.log(process.env.API_KEY);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+getDogImages(endpointDog);
