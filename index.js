05e919260d84b87c20a1c3c961b014e684c75db1
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-29 15:46:49 +0100

    Completed the Async Giphy task

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const endpointCat = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getCatImages(query) {
+  const response = await fetch(query);
+  const responseData = await response.json();
+
+  const singleGifImageURL = responseData.data[0].url;
+
+  console.log("Cat image: ", singleGifImageURL);
+}
+
+const endpointDog = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getDogImages(query) {
+  const response = await fetch(query);
+  const responseData = await response.json();
+
+  const singleGifImageURL = responseData.data[0].url;
+
+  console.log("Dog image: ", singleGifImageURL);
+}
+
+getCatImages(endpointCat);
+getDogImages(endpointDog);
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY);
